54a0085f3ce5589f718c30c20413f23aeaebc395
Sella Rafaeli Fiverr <sella.rafaeli@gmail.com>
2013-08-14 11:56:21 +0300

    Updated profile and added scoped status updates to it

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,13 @@
 class ProfilesController < ApplicationController
   def show
+    @user = User.find_by_profile_name(params[:id]) #look at the DB and find me a user by profile name
+    if @user
+      id = @user.id
+      #@statuses = @user.statuses.all
+      @statuses = Status.where(user_id:id)
+      render action: :show  
+    else 
+      render file: 'public/404', status: 404, formats: [:html]
+    end
   end
 end
