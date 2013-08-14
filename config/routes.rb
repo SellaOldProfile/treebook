ea2910e897148928cb9fa3185f4b580dd50b8419
Sella Rafaeli Fiverr <sella.rafaeli@gmail.com>
2013-08-14 11:55:17 +0300

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Treebook::Application.routes.draw do
 
   root to: 'statuses#index' # 'controller#action'
 
+  get '/:id', to: 'profiles#show' 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
